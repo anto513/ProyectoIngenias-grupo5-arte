f4340cb5034223bc2a55d0b6f38fb59768c148cf
anto513 <antonela.testa13@gmail.com>
2022-09-29 19:25:41 -0300

    agregue javascript 2

---
+++ main.js
@@ -0,0 +1,43 @@
+//SUSCRIBIRSE INICIO
+let nombreUsuario = prompt("Ingrese su nombre")
+let nombreEmail = prompt("Ingrese su email")
+let Edad = prompt("Ingrese su edad")
+alert("Bienvenidx a nuestra pagina " + nombreUsuario)
+console.log(nombreUsuario.nombreEmail.Edad);
+
+//PRODUCTOS
+/*class Producto {
+  constructor(nombre, precio, envio) {
+    this.nombre = nombre.toUpperCase();
+    this.precio = parseFloat(precio);
+    this.envio = envio.toUpperCase();
+    this.vendido = false;
+  }
+  mostrar() {
+    alert("El producto:" + this.nombre + "cuesta $" + this.precio)
+
+    envio() {
+      this.precio = this.precio * 300
+    }
+    vender() {
+      this.vendido = true;
+    }
+  }
+  const producto1 = new Producto("aros marrones", "300");
+  const producto2 = new Producto("aros violeta", "300");
+  const producto3 = new Producto("aros turquesa", "300");
+  const producto4 = new Producto("aros cuadrados", "300");
+  const producto5 = new Producto("peluche de pollo", "1500");
+  const producto6 = new Producto("peluche de oveja", "1500");
+  const producto5 = new Producto("peluche de conejo", "1500");
+  const producto6 = new Producto("peluche de palta", "1500");
+producto1.envio();
+producto2.envio();
+producto3.envio();
+producto4.envio();
+producto5.envio();
+producto6.envio();
+
+//ENVIANOS TU CONSULTA
+let nombreUsuario = prompt("ingrese su nombre")
+let consulta = prompt("Escribenos tu consulta")*/
